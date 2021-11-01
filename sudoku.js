21a98b44a83dbeae080c9be7faa7c082a7bfe5ac
sidkhuntia <siddharthakhuntia@gmail.com>
2021-11-01 15:40:33 +0530

    check

--- sudoku.js
+++ sudoku.js
@@ -1,4 +1,16 @@
 //Initializing a 2D array of blank numbers
 
+//function to create a 2D array of blank numbers 9*9
+function createArray(rows, cols) {
+    var arr = [];
 
+    for (var i=0;i<rows;i++) {
+        arr[i] = [];
+        for (var j=0;j<cols;j++) {
+            arr[i][j] = 0;
+        }
+    }
+
+    return arr;
+}
 
